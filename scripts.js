618cba1292133c560d8fbbe27edfb70a3fc63f06
enriquejames <enrique.j.aceves@gmail.com>
2025-01-06 20:08:57 -0800

    updates made to scripts.js to reflect corrections

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     var toggle = document.querySelector(".switch");
     var switchSlider = toggle.querySelector(".switch-slider");
     var switchLabel = toggle.querySelector(".switch-label");
-    toggle.addEventListener("click", function(){
+
+    toggle.addEventListener("click", function () {
         document.body.classList.toggle("dark-mode");
-        if(document.body.classList.contains("dark-mode")){
+        if (document.body.classList.contains("dark-mode")) {
             switchSlider.classList.add("switch-slider-right");
             switchLabel.innerHTML = "Dark Mode";
         } else {
@@ -14,19 +15,22 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.getElementById("myModal");
-    var btn = document.getElementById("myBtn");
-    var span = document.getElementsByClassName("close")[0];
-    btn.onclick = function() {
+document.addEventListener("DOMContentLoaded", function () {
+    var modal = document.querySelector(".modal");
+    var btn = document.querySelector(".contact-me-button");
+    var span = document.querySelector(".close");
+
+    btn.onclick = function () {
         modal.style.display = "block";
-    }
-    span.onclick = function() {
+    };
+
+    span.onclick = function () {
         modal.style.display = "none";
-    }
-    window.onclick = function(event) {
-        if (event.target == modal) {
+    };
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
             modal.style.display = "none";
         }
-    }
+    };
 });
